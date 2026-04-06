9ad9b50bd2d636f83977433110d037731366eb3b
Liz Arinder <elizabeth.m.arinder@gmail.com>
2026-04-06 11:51:14 -0400

    feat: add habits page with server-side data fetching

---
+++ apps/web/src/app/habits/page.tsx
@@ -0,0 +1,16 @@
+import { getHabits } from "@/services/habits";
+
+export default async function HabitsPage() {
+	const habits = await getHabits();
+
+	return (
+		<main>
+			<h1>Habits Page</h1>
+			<ul>
+				{habits.map((habit) => (
+					<li key={habit.name}>{habit.name}</li>
+				))}
+			</ul>
+		</main>
+	);
+}
